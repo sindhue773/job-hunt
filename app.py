0f2bdae8650cd31af51866c87b76c0abd7a124f6
Sindhu <esindhu242@gmail.com>
2025-06-01 11:11:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app",  port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 

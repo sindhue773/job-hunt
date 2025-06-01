85539f019c40851e2c6cb515a8cd18b098a4886b
Sindhu <esindhu242@gmail.com>
2025-06-01 11:53:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from main import run
+import os 
 
 class Item(BaseModel):
     job_title: str
